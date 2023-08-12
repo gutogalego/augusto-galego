e282e584a98491cde548e848d069fc1ec70effca
Augusto Galego <augustomirandagalego@gmail.com>
2023-08-12 16:39:26 +0200

    .

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <div className="bg-slate-950 text-white sm:py-8">
+      <nav className=" flex items-center justify-between">
+        <div className="w-1/5" />
+
+        <h1 className="w-1/ flex-1 text-center text-lg font-medium text-white">
+          Augusto Galego&apos;s projects
+        </h1>
+
+        <div className="w-1/5" />
+        <div className="flex w-1/5 items-center justify-center space-x-5">
+          <Link
+            href="https://twitter.com/RealGalego"
+            className="text-xl"
+            target="_blank"
+          >
+            𝕏
+          </Link>
+          <Link
+            href="https://github.com/gutogalego"
+            className=""
+            target="_blank"
+          >
+            <Image
+              src={"/github.svg"}
+              alt="Github"
+              className="h-5 w-5"
+              width={0}
+              height={0}
+            />
+          </Link>
+        </div>
+        <div className="w-1/5" />
+      </nav>
+    </div>
+  );
+};
