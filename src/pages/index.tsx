ed11e23e9fef5ed8170c33c6ed5acc4ed7fad269
Augusto Galego <augustomirandagalego@gmail.com>
2023-08-12 16:45:55 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,22 +13,18 @@ export default function Home() {
       </Head>
       <Navbar />
 
-      <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-slate-950 to-indigo-950 py-20">
-        <div className="">
-          <div className="">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
+      <main className="flex min-h-screen w-screen flex-col items-center  bg-gradient-to-b from-slate-950 to-indigo-950 py-20">
+        <Link
+          className="flex w-3/5 flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+          href="https://www.roadmapinvestidor.com.br/"
+          target="_blank"
+        >
+          <h3 className="text-2xl font-bold">Roadmap Investidor →</h3>
+          <div className="text-lg">
+            An open source Roadmap, in Portuguese, that helps people in Brazil
+            to learn about investing
           </div>
-        </div>
+        </Link>
       </main>
     </>
   );
