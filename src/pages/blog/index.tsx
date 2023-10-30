fbc53f3e19e19b88160ac40f78f538f30164091c
Augusto Galego <agalego@joor.com>
2023-10-30 19:14:48 +0100

    .

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+      <div className="bg-neutral-100  text-neutral-900 sm:py-8 sm:px-64">
+        <nav className="flex items-center justify-between">
+          <div className="w-1/5" />
+  
+          <h1 className="w-1/ flex-1 text-center text-lg font-medium">
+            Augusto Galego&apos;s blog
+          </h1>
+  
+          <div className="w-1/5" />
+          <div className="flex w-1/5 items-center justify-center space-x-5">
+            <Link
+              href="https://twitter.com/RealGalego"
+              className="text-xl"
+              target="_blank"
+            >
+              𝕏
+            </Link>
+            <Link
+              href="https://github.com/gutogalego"
+              className=""
+              target="_blank"
+            >
+              <Image
+                src={"/github-dark.svg"}
+                alt="Github"
+                className="h-5 w-5"
+                width={0}
+                height={0}
+              />
+            </Link>
+          </div>
+          <div className="w-1/5" />
+        </nav>
+      </div>
+    );
+  };
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Augusto Galego | Blog</title>
+        <meta name="description" content="Galego's personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+
+      <main className="flex min-h-screen w-screen flex-col items-center  space-y-6 bg-neutral-100 py-20">
+       
+      </main>
+    </>
+  );
+}
