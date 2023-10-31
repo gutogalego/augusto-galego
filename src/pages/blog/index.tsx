509700ef9fe4800c2abd24415ef9def86a371d4c
Augusto Galego <augustomirandagalego@gmail.com>
2023-10-31 18:13:26 +0100

    .

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -15,7 +15,10 @@ const Navbar = () => {
 
         <div className="flex w-1/4 items-center justify-center space-x-5">
           <Link
+
             href="https://twitter.com/RealGalego"
+
+            
             className="text-xl"
             target="_blank"
           >
