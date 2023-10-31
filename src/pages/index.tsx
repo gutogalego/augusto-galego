2dcb5b66eabf6a869e1b958d1497174029221735
Augusto Galego <augustomirandagalego@gmail.com>
2023-10-31 21:25:37 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,15 @@ export default function Home() {
       <main className="flex min-h-screen w-screen flex-col items-center  space-y-6 bg-gradient-to-b from-slate-950 to-indigo-950 py-20">
         <Link
           className="link flex w-3/5 flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+          href="/blog"
+        >
+          <h3 className="text-2xl font-bold">Blog (2023) →</h3>
+          <div className="text-lg">
+            As every dev, I reinvented the wheel of blogging
+          </div>
+        </Link>
+        <Link
+          className="link flex w-3/5 flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
           href="/ui/dashboard"
         >
           <h3 className="text-2xl font-bold">UI (2023) →</h3>
