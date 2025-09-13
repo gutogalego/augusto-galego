46a5ef72dea8bd6d59df792e8874ed6b4579838d
Vinicius Rocha <work@hayato.me>
2025-09-13 03:27:49 -0300

    chore: migrate ESLint and Prettier configurations to Biome, update package dependencies, and clean up project structure

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
-import withMDX from '@next/mdx';
+await import('./src/env.mjs')
+import withMDX from '@next/mdx'
 
 const mdx = withMDX({
-  extension: /\.mdx?$/
-});
+  extension: /\.mdx?$/,
+})
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -20,8 +20,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
     remotePatterns: [
@@ -31,5 +31,5 @@ const config = {
       },
     ],
   },
-};
+}
 export default mdx(config)
